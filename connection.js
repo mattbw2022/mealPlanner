80b0f208b216247955569539a913c5ee85cb0365
mattbw2022 <matthewbward123@gmail.com>
2023-10-12 15:15:48 -0500

    fix connection

--- connection.js
+++ connection.js
@@ -1,21 +1,21 @@
-// const Pool = require('pg').Pool
-// const pool = new Pool({
-//   user: process.env.DB_USERNAME,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-// });
-
 const Pool = require('pg').Pool
-const pool = new Pool ({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: 5432
-
+  port: process.env.DB_PORT,
 });
+
+// const Pool = require('pg').Pool
+// const pool = new Pool ({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: process.env.DB_PASSWORD,
+//   port: 5432
+
+// });
 
 
 module.exports = pool;
