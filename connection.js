b52d4c7447dfae921ddc2d3d8bf022bcff12644e
mattbw2022 <matthewbward123@gmail.com>
2023-10-12 12:29:46 -0500

    update

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: DB_USERNAME,
+  user: process.env.DB_USERNAME,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
